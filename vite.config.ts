3250193e7da268e819c8f50a5113e4d8c8b3c523
hitesh78766 <michitesh6@gmail.com>
2026-08-14 22:38:17 +0530

    solve thei issue for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Hitesh_Portfolio/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
